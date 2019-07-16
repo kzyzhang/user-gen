0fbd5e3399b67fb627ab05c7536d61397502a06b
kzyzhang <kz283@cam.ac.uk>
2019-07-16 13:31:16 +0100

    abstracted validation and clean up

--- user_gen.py
+++ user_gen.py
@@ -7,19 +7,6 @@ import sys
 date = datetime.datetime.now()
 fake = Faker("en_GB")
 
-# user = {
-#     "username": "",
-#     "name": "",
-#     "age": "",
-#     "DOB": "",
-#     "favourite colour": "",
-#     "address": "",
-#     "postcode": "",
-#     "email": "",
-#     "phone number": "",
-# }
-# print(user)
-
 
 def age_calculate(DOB, date):
     if date.month == DOB.month:
@@ -35,41 +22,41 @@ def age_calculate(DOB, date):
 
 def user_gen(no_users):
     users = []
-    p = re.compile("^[0-9]+$")
-    # return p.match(str(no_users))
-    if p.match(str(no_users)) != None:
 
-        for v in range(int(no_users)):
+    for v in range(no_users):
 
-            DOB = fake.date_of_birth(
-                tzinfo=None, minimum_age=0, maximum_age=115
-            )
-            DOB_str = DOB.strftime("%d-%m-%Y")
-            user = {
-                "name": fake.name(),
-                "birthday": DOB_str,
-                "age": age_calculate(DOB, date),
-                "favourite colour": fake.color_name(),
-                "address": fake.street_address(),
-                "postcode": fake.postcode(),
-                "email": fake.email(),
-                "phone number": fake.phone_number(),
-                "username": fake.user_name(),
-            }
+        DOB = fake.date_of_birth(minimum_age=0, maximum_age=115)
+        DOB_str = DOB.strftime("%d-%m-%Y")
+        user = {
+            "name": fake.name(),
+            "birthday": DOB_str,
+            "age": age_calculate(DOB, date),
+            "favourite colour": fake.color_name(),
+            "address": fake.street_address(),
+            "postcode": fake.postcode(),
+            "email": fake.email(),
+            "phone number": fake.phone_number(),
+            "username": fake.user_name(),
+        }
 
-            users.append(user)
-        return users
+        users.append(user)
+    return users
 
+
+def validate_input(input):
+    regex = re.compile("^[0-9]+$")
+
+    if regex.match(input) is not None:
+        pass
     else:
-        return "this is an invalid number of users"
+        print(f"{input} not a valid number")
+        sys.exit(1)
 
 
-# print(user_gen("2ef"))
 if __name__ == "__main__":
-    # print(sys.argv[1])
-    user_lst = user_gen((sys.argv[1]))
-    print(user_lst)
-# DOB = fake.date_of_birth(tzinfo=None, minimum_age=0, maximum_age=115)
-# print(type(DOB))
-#   fake.user_name(*args, **kwargs)
+    input = sys.argv[1]
+    validate_input(input)
+    quantity = int(input)
+    users = user_gen(quantity)
+    print(users)
 
